2734aec7b90f269893f02ca309a527fcefa422eb
Thornton Prime <thornton.prime@gmail.com>
2022-03-01 14:03:15 -0800

    Updated text parser

--- parsetext.py
+++ parsetext.py
@@ -1,95 +1,125 @@
 #!/usr/bin/env python
 
-
-from sys import stdin
-
-import unicodedata
-
-from slugify import slugify
-
-pos = 0
-
-nword = 0
-word = ""
-word_pos = 0
-
-nline = 0
-line = ""
-line_pos = 0
-
-nblok = 0
-blok_pos = 0
-blok = ""
-
-nsent = 0
-sent_pos = 0
-sent = ""
+from unicodedata import category as unicodedata_category
+from typing import TextIO
 
 
-while c := stdin.read(1):
-   
-    category = unicodedata.category(c)
-    if len(category) > 2:
-        print(c, category)
-        raise Exception("Weird character!")
 
-    ordinal = ord(c)
- 
-    match category[0]:
 
-        # Letter
-        case "L": 
+def get_text_data(fd: TextIO):
+    """
+    """
 
-            # New word
-            if not word:
-                word_pos = pos
+    # Set up positional markers and slots
 
-            word += c
-            line += c
-            sent += c
+    pos = 0
 
-        # Separator
-        case "M" | "N" | "P" | "S" | "Z" |"C":
+    nword = 0
+    word = ""
+    word_pos = 0
 
-            # Connectors ... ignore
-            if ( word and category == "Cf" ) or ( word and category == "Pc" ):
-                pass
+    nsent = 0
+    sent_pos = 0
+    sent = ""
 
-            # Sentence Breaker
-            elif category == "Po":
-                sent += c
+    nline = 0
+    line = ""
+    line_pos = 0
+
+    nblok = 0
+    blok_pos = 0
+    blok = ""
+
+
+    while c := fd.read(1):
+       
+        category = unicodedata_category(c)
+        if len(category) > 2:
+            raise Exception(f"Weird character! {c}")
+
+        ordinal = ord(c)
+     
+        match category[0]:
+
+            # Letter
+            case "L": 
+
+                # New word
+                if not word:
+                    word_pos = pos
+
+                word += c
                 line += c
-                
-                if sent.strip():
-                    nsent += 1
-                    print("SENT :", sent_pos, nsent, sent)
-                    sent = ""
 
-            # Control character
-            elif ordinal in [ 0x0d, 0x0a ]:
-                
-                if line.strip():
-                    nline += 1
-                    print("LINE :", line_pos, nline, line)
-                    line = ""
+                if not sent:
+                    sent_pos = pos
+                sent += c
+
+            # Separator
+            case "M" | "N" | "P" | "S" | "Z" |"C":
+
+                # Connectors ... ignore
+                if ( word and category == "Cf" ) or ( word and category == "Pc" ):
+                    pass
+
+                # Sentence Breaker
+                elif category == "Po":
+                    sent += c
+                    line += c
+                    
+                    if  c in '.?!¿¡'  and sent.strip():
+                        nsent += 1
+                        yield "SENT", sent_pos, nsent, sent
+                        sent = ""
+
+                # Control character
+                elif ordinal in [ 0x0d, 0x0a ]:
+                    
+                    if line.strip():
+                        nline += 1
+                        yield "LINE", line_pos, nline, line
+                        line = ""
+
+                    else:
+                        nblok += 1
+                        yield "BLOK", blok_pos, nblok, blok
+                        blok = ""
+                        blok_pos = pos + 1
+     
+                    line_pos = pos + 1
+
+                    if sent:
+                        sent += " "
 
                 else:
-                    print("BLOCK")
-                    blok = ""
-                
-                sent += " "
-                line_pos = pos + 1
-
-            else:
-                if word:
-                    nword += 1
-                    print("WORD :", word_pos, nword, word)
-                    word = ""
-
-                if line:
                     line += c
 
-                if sent:
-                    sent += c
+                    if sent:
+                        sent += c
 
-    pos += 1
+                # Handle word and sent
+                if word:
+                    nword += 1
+                    yield "WORD", word_pos, nword, word
+                    word = ""
+
+
+        pos += 1
+        blok += c
+
+    if sent.strip():
+        yield "SENT", sent_pos, nsent, sent
+    if word:
+        yield "WORD", word_pos, nword, word
+    if line.strip():
+        yield "LINE", line_pos, nline, line
+    if blok:
+        yield "BLOK", blok_pos, nblok, blok
+
+
+if __name__ == "__main__":
+    
+    from sys import stdin
+
+    for d in get_text_data(stdin):
+        print(d)
